84a84c25d010a652e81fe5208f8e137349a6b1e2
Radwa <radwasedeek2013@gmail.com>
2024-08-07 14:43:39 +0300

    add recycle view to home fragment and display data

--- app/src/main/java/com/example/recipeapp/HomeFragment.kt
+++ app/src/main/java/com/example/recipeapp/HomeFragment.kt
@@ -5,9 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recipeapp.network.APIClient
+import com.example.recipeapp.network.RetrofitService
+import com.example.recipeapp.repo.RecipeRepositoryImplementation
+import com.example.recipeapp.viewModel.RecipeViewModel
+import com.example.recipeapp.viewModel.RecipeViewModelFactory
 
 class HomeFragment : Fragment() {
 
+    lateinit var recyclerView: RecyclerView
+    lateinit var myAdapter: MyRecipeAdapter
+    lateinit var recipeViewModel : RecipeViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +32,30 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recyclerView = view.findViewById(R.id.recipeList)
+
+        myAdapter = MyRecipeAdapter(requireContext())
+        val layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+
+        recyclerView.adapter = myAdapter
+        recyclerView.layoutManager = layoutManager
+        getViewModel()
+        recipeViewModel.getAllMeals()
+        recipeViewModel.listOfMeals?.observe(viewLifecycleOwner, Observer {
+            myAdapter.setMeals(it.meals)
+        })
+    }
+
+    private fun getViewModel(){
+        val recipeViewModelFactory = RecipeViewModelFactory(
+            RecipeRepositoryImplementation(APIClient)
+        )
+        recipeViewModel = ViewModelProvider(this , recipeViewModelFactory).get(RecipeViewModel::class.java)
     }
 
 
